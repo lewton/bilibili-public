facc198fc4c24550684d1578e3ed283aee4d9538
lewo <420092586@qq.com>
2023-10-12 00:13:30 +0800

    JavaScript实现生命游戏

--- JavaScript/5-生命游戏/script.js
+++ JavaScript/5-生命游戏/script.js
@@ -1,12 +1,17 @@
 const gridContainer = document.getElementById("grid-container");
 
+// 网格行数
 const numRows = 20;
+// 网格列数
 const numCols = 20;
+// 单元格尺寸
 const cellSize = 20;
+// 单元格DOM缓存
 const cellMap = {};
+
 let grid = new Array(numRows);
 
-// 创建初始网格
+// 创建初始化网格
 function createGrid() {
   for (let i = 0; i < numRows; i++) {
     grid[i] = new Array(numCols).fill(0);
@@ -20,8 +25,8 @@ function createGrid() {
       cell.style.width = cellSize + "px";
       cell.style.height = cellSize + "px";
       cell.className = grid[i][j] ? "cell alive" : "cell dead";
-      cell.dataset.i = i;
-      cell.dataset.j = j;
+      cell.dataset.i = `${i}`;
+      cell.dataset.j = `${j}`;
       gridContainer.appendChild(cell);
       // 缓存 cell dom
       cellMap[`${i}-${j}`] = cell;
@@ -48,12 +53,14 @@ function renderGrid() {
 // 启动生命游戏
 let intervalId = null;
 function startGame() {
-  intervalId = setInterval(updateGrid, 200); // 设置更新网格的定时器，每200毫秒更新一次
+  // 设置更新网格的定时器，每200毫秒更新一次
+  intervalId = setInterval(updateGrid, 200);
 }
 
 // 停止生命游戏
 function stopGame() {
-  clearInterval(intervalId); // 清除定时器，停止游戏循环
+  // 清除定时器，停止游戏循环
+  intervalId && clearInterval(intervalId);
 }
 
 // 清空网格
@@ -63,22 +70,23 @@ function clearGrid() {
       grid[i][j] = 0;
     }
   }
-  renderGrid(); // 重新渲染网格
+
+  renderGrid();
 }
 
-// 更新网格状态
+// 更新网络状态
 function updateGrid() {
   const newGrid = new Array(numRows);
-
   for (let i = 0; i < numRows; i++) {
     newGrid[i] = new Array(numCols).fill(0);
   }
 
   /*
-    如果一个活细胞周围有2或3个活细胞，它将继续存活。
-    如果一个活细胞周围有少于2个活细胞，它会因孤独而死亡。
-    如果一个活细胞周围有超过3个活细胞，它会因拥挤而死亡。
-    如果一个死细胞周围有恰好3个活细胞，它将复活。
+  生命游戏规则：
+      如果一个活细胞周围有2或3个活细胞，它将继续存活。
+      如果一个活细胞周围有少于2个活细胞，它会因孤独而死亡。
+      如果一个活细胞周围有超过3个活细胞，它会因拥挤而死亡。
+      如果一个死细胞周围有恰好3个活细胞，它将复活。
   */
   for (let i = 0; i < numRows; i++) {
     for (let j = 0; j < numCols; j++) {
@@ -100,7 +108,8 @@ function updateGrid() {
   }
 
   grid = newGrid;
-  renderGrid(); // 更新显示
+  // 更新显示
+  renderGrid();
 }
 
 // 计算细胞周围的活细胞数
@@ -120,6 +129,8 @@ function countNeighbors(x, y) {
   for (const offset of offsets) {
     const neighborX = x + offset[0];
     const neighborY = y + offset[1];
+
+    // 保证细胞动画再指定范围运动
     if (
       neighborX >= 0 &&
       neighborX < numRows &&
@@ -135,8 +146,8 @@ function countNeighbors(x, y) {
 
 // 切换单元格状态
 function toggleCellState(cell) {
-  const i = cell.dataset.i;
-  const j = cell.dataset.j;
+  const i = +cell.dataset.i;
+  const j = +cell.dataset.j;
   grid[i][j] = 1 - grid[i][j];
   cell.className = grid[i][j] ? "cell alive" : "cell dead";
 }
